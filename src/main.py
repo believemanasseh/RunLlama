414db573645259c3589d767657a580f08b1f51d4
Manasseh <believemanasseh@gmail.com>
2025-02-06 23:04:34 +0100

    feat: add package version option

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import click
-from ollama import ChatResponse, ResponseError
+from ollama import ResponseError
 from ollama import pull as pull_model
 
+from . import PROJECT_VERSION
 from .helpers import get_chat_response
 
 
 @click.group()
+@click.version_option(
+    PROJECT_VERSION, "--version", "-v", message="RunLlama v%(version)s"
+)
 def runner():
     """Large language model runner"""
     pass
@@ -20,12 +24,12 @@ def query(model: str, prompt: str) -> None:
     """Query LLM"""
 
     try:
-        response: ChatResponse = get_chat_response(model, prompt)
+        response = get_chat_response(model, prompt)
     except ResponseError as e:
         click.echo(e)
         if e.status_code == 404:
             pull_model(model)  # Pulls model from registry
-            response: ChatResponse = get_chat_response(model, prompt)
+            response = get_chat_response(model, prompt)
     except Exception as e:
         click.echo(e)
         exit()
