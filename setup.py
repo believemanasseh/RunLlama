1a4d86518ccad11268b461d875f85c618ba49c0c
Manasseh <believemanasseh@gmail.com>
2025-02-07 08:23:26 +0100

    feat: bump package to version 0.1.1

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
+"""Setup for packaging and distribution."""
+
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+with open("README.md", encoding="utf-8") as fh:
+	long_description = fh.read()
 
 setup(
-    name="runllama",
-    version="0.1.0",
-    author="Believe Manasseh",
-    author_email="believemanasseh@gmail.com",
-    description="Simple CLI runner for Ollama (LLM) models",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=["src"],
-    install_requires=["ollama>=0.4.7", "click>=8.0.0"],
-    classifiers=[
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
-        "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha",
-    ],
-    python_requires=">=3.10",
+	name="runllama",
+	version="0.1.1",
+	author="Believe Manasseh",
+	author_email="believemanasseh@gmail.com",
+	description="Simple CLI runner for Ollama (LLM) models",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=["src"],
+	install_requires=["ollama>=0.4.7", "click>=8.0.0"],
+	classifiers=[
+		"Programming Language :: Python :: 3.10",
+		"Programming Language :: Python :: 3.11",
+		"Programming Language :: Python :: 3.12",
+		"Programming Language :: Python :: 3.13",
+		"Programming Language :: Python :: 3.14",
+		"Operating System :: OS Independent",
+		"Development Status :: 3 - Alpha",
+	],
+	python_requires=">=3.10",
 )
--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
+"""This module contains base logic for all RunLlama actions."""
+
+import sys
 import time
+from http import HTTPStatus
 
 import click
 from ollama import ResponseError
@@ -9,55 +13,49 @@ from .helpers import get_chat_response
 
 
 @click.group()
-@click.version_option(
-    PROJECT_VERSION, "--version", "-v", message="RunLlama v%(version)s"
-)
-def runner():
-    """Large language model runner"""
-    pass
+@click.version_option(PROJECT_VERSION, "--version", "-v", message="RunLlama v%(version)s")
+def runner() -> None:
+	"""Large language model runner"""
 
 
 @runner.command()
-@click.option(
-    "--model", "-m", type=str, default="deepseek-r1:1.5b", help="Name of model"
-)
+@click.option("--model", "-m", type=str, default="deepseek-r1:1.5b", help="Name of model")
 @click.argument("prompt", type=str)
 def query(model: str, prompt: str) -> None:
-    """Query LLM"""
+	"""Query LLM"""
+	try:
+		response = get_chat_response(model, prompt)
+	except ResponseError as e:
+		click.echo(e)
+		if e.status_code == HTTPStatus.NOT_FOUND:
+			click.echo(f"Pulling model [{model}]...")
+			pull_model(model)  # Pulls model from registry
+			time.sleep(5)
+			try:
+				response = get_chat_response(model, prompt)
+			except ResponseError as e:
+				click.echo(e)
+				click.echo("Exiting...")
+				sys.exit(1)
+	except Exception as e:
+		click.echo(e)
+		sys.exit(1)
 
-    try:
-        response = get_chat_response(model, prompt)
-    except ResponseError as e:
-        click.echo(e)
-        if e.status_code == 404:
-            click.echo(f"Pulling model [{model}]...")
-            pull_model(model)  # Pulls model from registry
-            time.sleep(5)
-            try:
-                response = get_chat_response(model, prompt)
-            except ResponseError as e:
-                click.echo(e)
-                click.echo("Exiting...")
-                exit(1)
-    except Exception as e:
-        click.echo(e)
-        exit(1)
-
-    click.echo(response["message"]["content"])
+	click.echo(response["message"]["content"])
 
 
 @runner.command()
 @click.argument("model", type=str)
-def pull(model: str):
-    """Pull a model from registry"""
-    try:
-        click.echo(f"Pulling model: {model}")
-        pull_model(model)
-        click.echo("Model pulled successfully!")
-    except Exception as e:
-        click.echo(e)
-        exit(1)
+def pull(model: str) -> None:
+	"""Pull a model from registry"""
+	try:
+		click.echo(f"Pulling model: {model}")
+		pull_model(model)
+		click.echo("Model pulled successfully!")
+	except Exception as e:
+		click.echo(e)
+		sys.exit(1)
 
 
 if __name__ == "__main__":
-    runner()
+	runner()
